413fef51c107981c5044d182a41d63ddc18b93a6
Haidar Mehsen <hmehsen@acksession.com>
2024-02-17 19:56:59 +0200

    Fixed paths of some assets

--- lib/src/image_paths.dart
+++ lib/src/image_paths.dart
@@ -1,8 +1,8 @@
 final class ImagePaths {
   static const String appleSignInButtonIcon =
-      'lib/assets/svg_vec/apple_sign_in_button_icon.svg.vec';
+      'packages/social_media_sign_in_buttons/lib/assets/svg_vec/apple_sign_in_button_icon.svg.vec';
   static const String facebookSignInButtonIcon =
-      'lib/assets/svg_vec/facebook_sign_in_button_icon.svg.vec';
+      'packages/social_media_sign_in_buttons/lib/assets/svg_vec/facebook_sign_in_button_icon.svg.vec';
   static const String googleSignInButtonIcon =
-      'lib/assets/svg_vec/google_sign_in_button_icon.svg.vec';
+      'packages/social_media_sign_in_buttons/lib/assets/svg_vec/google_sign_in_button_icon.svg.vec';
 }
