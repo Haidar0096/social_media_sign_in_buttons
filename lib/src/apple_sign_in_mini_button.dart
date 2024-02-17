0097b0df3a1636cc8002ae7554f280bc2a67d286
Haidar Mehsen <hmehsen@acksession.com>
2024-02-17 21:01:47 +0200

    Added options to customize the buttons

--- lib/src/apple_sign_in_mini_button.dart
+++ lib/src/apple_sign_in_mini_button.dart
@@ -3,23 +3,45 @@ import 'package:flutter/material.dart';
 import 'image_paths.dart';
 import 'svg_icon_button.dart';
 
+/// Apple sign-in mini button.
 class AppleSignInMiniButton extends StatelessWidget {
   final void Function() onPressed;
-  final double? width;
-  final double? height;
+  final ButtonStyle? style;
+  final MaterialStatesController? statesController;
+  final bool autofocus;
+  final Clip clipBehavior;
+  final FocusNode? focusNode;
+  final ValueChanged<bool>? onFocusChange;
+  final ValueChanged<bool>? onHover;
+  final VoidCallback? onLongPress;
+  final BoxFit? iconFit;
 
   const AppleSignInMiniButton({
     super.key,
     required this.onPressed,
-    this.width,
-    this.height,
+    this.style,
+    this.statesController,
+    this.autofocus = false,
+    this.clipBehavior = Clip.none,
+    this.focusNode,
+    this.onFocusChange,
+    this.onHover,
+    this.onLongPress,
+    this.iconFit,
   });
 
   @override
   Widget build(BuildContext context) => SvgIconButton(
-        onPressed: onPressed,
         svgVectorPath: ImagePaths.appleSignInButtonIcon,
-        width: width,
-        height: height,
+        onPressed: onPressed,
+        style: style,
+        statesController: statesController,
+        autofocus: autofocus,
+        clipBehavior: clipBehavior,
+        focusNode: focusNode,
+        onFocusChange: onFocusChange,
+        onHover: onHover,
+        onLongPress: onLongPress,
+        iconFit: iconFit,
       );
 }
--- lib/src/svg_icon_button.dart
+++ lib/src/svg_icon_button.dart
@@ -4,26 +4,47 @@ import 'package:vector_graphics/vector_graphics.dart';
 
 /// A button that displays an SVG icon.
 class SvgIconButton extends StatelessWidget {
-  final void Function() onPressed;
-  final double? width;
-  final double? height;
   final String svgVectorPath;
+  final void Function() onPressed;
+  final ButtonStyle? style;
+  final MaterialStatesController? statesController;
+  final bool autofocus;
+  final Clip clipBehavior;
+  final FocusNode? focusNode;
+  final ValueChanged<bool>? onFocusChange;
+  final ValueChanged<bool>? onHover;
+  final VoidCallback? onLongPress;
+  final BoxFit? iconFit;
 
   const SvgIconButton({
     super.key,
-    required this.onPressed,
-    this.width,
-    this.height,
     required this.svgVectorPath,
+    required this.onPressed,
+    this.style,
+    this.statesController,
+    this.autofocus = false,
+    this.clipBehavior = Clip.none,
+    this.focusNode,
+    this.onFocusChange,
+    this.onHover,
+    this.onLongPress,
+    this.iconFit,
   });
 
   @override
   Widget build(BuildContext context) => ElevatedButton(
         onPressed: onPressed,
+        style: style,
+        statesController: statesController,
+        autofocus: autofocus,
+        clipBehavior: clipBehavior,
+        focusNode: focusNode,
+        onFocusChange: onFocusChange,
+        onHover: onHover,
+        onLongPress: onLongPress,
         child: SvgPicture(
           AssetBytesLoader(svgVectorPath),
-          width: width,
-          height: height,
+          fit: iconFit ?? BoxFit.contain,
         ),
       );
 }
